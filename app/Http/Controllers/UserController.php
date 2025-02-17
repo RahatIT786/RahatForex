25c3eb895b72cf4861e6fd8b890de26931cb9174
RahatIT786 <rahatit486@gmail.com>
2025-02-17 20:04:15 +0530

    optimize added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\MoneyForex;
 use App\Mail\ForexQuoteRequest;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Artisan;
 
 class UserController extends Controller
 {
@@ -53,6 +54,31 @@ public function store(Request $request)
 
     return back()->with('success', 'Your message has been sent!');
 }
+
+
+public function clearCache(){
+    Artisan::call('optimize');
+    return "DEAR ADMIN YOUR APPLICATION CACHES ARE CLEARED !";
+}
+
+public function migrate(){
+    try {
+        // Capture the output from the Artisan command
+        Artisan::call('migrate', ['--force' => true]); // '--force' to run without confirmation in production
+
+        // Get the output as a string
+        $outputString = Artisan::output();
+
+        // Return the output to the view or as a response
+        // return response()->json(['status' => 'success', 'message' => $outputString]);
+        return $outputString;
+    } catch (\Exception $e) {
+        // Catch any errors and return them
+       return $e->getMessage();
+        // return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+    }
+}
+
 
 
 }
